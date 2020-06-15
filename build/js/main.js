3713aefb0a2687f437f14264c943138ed31b3e02
Jurij <zaitsev.jurij@gmail.com>
2020-06-15 10:36:19 +0300

    Правки стилей и скриптов

--- build/js/main.js
+++ build/js/main.js
@@ -2,63 +2,97 @@
   //мобильное меню
   var navToggler = document.querySelector(".main-nav__toggler-button");
   var navMenu = document.querySelector(".main-nav");
+  var navList = document.querySelector(".main-nav__list");
 
   document.querySelector(".nojs").classList.remove("nojs");
 
   navToggler.addEventListener("click", function () {
-    navMenu.classList.toggle("main-nav--open");
+    menuStateChange();
   })
 
-// прокрутка страницы
-  $('a[href^="#"]').on('click',function (e) {
+  navList.onclick = function(e) {
     e.preventDefault();
+    var target = e.target;
 
-    var target = this.hash;
-    var $target = $(target);
+    if (target.tagName != "A") return;
+    menuStateChange();
+    var hash = target.href.replace(/[^#]*(.*)/, '$1');
+    pageScroll(hash);
+  }
 
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
-    }, 400, 'swing', function () {
-        window.location.hash = target;
-    });
-  });
+
+  var menuStateChange= function() {
+    navMenu.classList.toggle("main-nav--open");
+  }
+
+//скроллинг страницы
+  var pageScroll = function(anchor) {
+    var destination = document.querySelector(anchor);
+    var start = null;
+    var distance = destination.getBoundingClientRect().top;
+    var v = 0.5;
+    requestAnimationFrame(step);
+
+
+    function step(t) {
+      if (!start) start = t;
+
+      var progress = t - start;
+      var r = Math.min(progress/v, distance);
+
+      console.log(r);
+      window.scrollTo(0,r);
+
+      if (r != distance) {
+          requestAnimationFrame(step)
+      }
+    }
+
+  }
+
 
   //видео
-  var tag = document.createElement("script");
-  tag.src = "//www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName("script")[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-  var player;
-
-  onYouTubeIframeAPIReady = function () {
-      player = new YT.Player("player", {
-          height: "100%",
-          width: "100%",
-          videoId: "_T7NTe3uBN4",
-          playerVars: {
-              "autoplay": 0,
-              "rel": 0,
-              "showinfo": 0
-          },
-          events: {
-              "onStateChange": onPlayerStateChange
-          }
-      });
+  var hide = function(element) {
+    element.classList.add("visually-hidden");
   }
 
-  var p = document.getElementById ("player");
-  $(p).hide();
 
-  onPlayerStateChange = function (event) {
-      if (event.data == YT.PlayerState.ENDED) {
-          $(".video__button").fadeIn("normal");
-      }
+  var createYTPlayer = function(){
+    var tag = document.createElement("script");
+    tag.src = "//www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName("script")[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    var player;
+
+    onYouTubeIframeAPIReady = function () {
+        player = new YT.Player("player", {
+            height: "100%",
+            width: "100%",
+            videoId: "_T7NTe3uBN4",
+            playerVars: {
+                "autoplay": 0,
+                "rel": 0,
+                "showinfo": 0
+            },
+            events: {
+              'onReady': onPlayerReady,
+            }
+        });
+    }
+
+    function onPlayerReady(event) {
+      event.target.playVideo();
+    }
   }
 
-  $(document).on("click", ".video__button", function () {
-      $(this).hide();
-      $("#player").show();
-      player.playVideo();
-  });
+  var playButton = document.querySelector(".video__button");
+
+  playButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    createYTPlayer();
+    playButton.hidden = true;
+
+   })
+
 })();
